f0a43fabc875d53cee13e16bdf6b500716cab3ad
Sofiane ISSOUFOU ISSAKA <87572695+Pheonix64@users.noreply.github.com>
2023-08-06 20:44:47 +0000

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,7 +2,7 @@ import motor.motor_asyncio
 from decouple import config
 
 def connect_db():
-    DATABASE_URL = config("DATABASE_URL")
+    DATABASE_URL = "mongodb+srv://libraire:Azval227@clusterbooks.fdeazkj.mongodb.net/?retryWrites=true&w=majority"
     DATABASE_NAME = "bookshop"
    # client = pymongo.MongoClient(DATABASE_URL)
 
