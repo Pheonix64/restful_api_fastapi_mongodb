112fef19d2f5942311fbfce5ef380ca1f1b3be7e
Sofiane ISSOUFOU ISSAKA <87572695+Pheonix64@users.noreply.github.com>
2023-08-11 15:25:05 +0000

    Update database.py

--- app/database.py
+++ app/database.py
@@ -2,8 +2,7 @@ import motor.motor_asyncio
 from decouple import config
 
 def connect_db():
-    #config("DATABASE_URL")
-    DATABASE_URL = "mongodb+srv://libraire:Azval227@clusterbooks.fdeazkj.mongodb.net/?retryWrites=true&w=majority"
+    DATABASE_URL = config("DATABASE_URL")
     # client = pymongo.MongoClient(DATABASE_URL)
     client = motor.motor_asyncio.AsyncIOMotorClient(DATABASE_URL) 
     print('🚀 Connected to MongoDB...')
